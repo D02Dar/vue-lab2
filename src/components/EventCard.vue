fd85412ea569809cb0995ded6dac958af2fac0be
D02Dar <wentao_dai@cmu.ac.th>
2025-07-03 22:58:07 +0700

    7

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,34 +1,25 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import type { Event } from '../types'
+defineProps<{ event: Event }>()
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>{{ event.time }} on {{ event.date }}</span>
+  </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
 }
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
